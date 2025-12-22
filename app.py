bea675b158f8c81e6539bf793ba8948aeeeb1bd4
Sheikh Abdul Halim <trainee.sheikh.abdul.halim@sigmalabs.co.uk>
2025-12-22 13:16:48 +0000

    Add Flask to app.py with endpoint to homepage. Move index.html to templates.

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 """Application for growth in different metric for countries."""
 
 from os import environ
-from dotenv import load_dotenv  # Loads variables from a file into the environment
+from dotenv import load_dotenv
 from flask import Flask, jsonify, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
