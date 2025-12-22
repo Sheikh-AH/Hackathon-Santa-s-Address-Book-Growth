59f6b2246757395538bb06647d4ab606950e7f03
Sheikh Abdul Halim <trainee.sheikh.abdul.halim@sigmalabs.co.uk>
2025-12-22 14:16:08 +0000

    Fix countries template file name.

--- app.py
+++ app.py
@@ -16,13 +16,13 @@ all_countries = remove_before_1900(df)
 # JSON Endpoints
 
 
-@app.route('/api/countries/<country_code>')
-def get_country(country_code):
-    # Get all data for specified country
-    country_data = all_countries[all_countries['code'] == country_code]
-    country_data = country_data.to_dict('records')
+# @app.route('/api/countries/<country_code>')
+# def get_country(country_code):
+#     # Get all data for specified country
+#     country_data = all_countries[all_countries['code'] == country_code]
+#     country_data = country_data.to_dict('records')
 
-    return country_data
+#     return country_data
 
 
 # Template Endpoints (HTML Pages)
@@ -37,7 +37,7 @@ def home():
 def country_page(country_name):
     data = get_data_of_country(country_name)
 
-    return render_template('chartexample.html',
+    return render_template('countries.html',
                            title=country_name,
                            labels=list(data["Year"]),
                            data=list(data["Population (historical)"]))
